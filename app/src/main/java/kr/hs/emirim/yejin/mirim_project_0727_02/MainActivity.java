891d384a473e5aa0b45f8bf9295b86a2081ea1e8
이 예진 <s2108@e-mirim.hs.kr>
2022-07-27 11:43:04 +0900

    1-2

--- app/src/main/java/kr/hs/emirim/yejin/mirim_project_0727_02/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/yejin/mirim_project_0727_02/MainActivity.java
@@ -1,14 +1,64 @@
 package kr.hs.emirim.yejin.mirim_project_0727_02;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textName, textEmail;
+    EditText dlgEditName, dlgEditEmail;
+    TextView toastText;
+    View dlgView, toastView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textName = findViewById(R.id.text_name);
+        textEmail = findViewById(R.id.text_email);
+
+        Button btnInput = findViewById(R.id.btn_input);
+        btnInput.setOnClickListener(btnInputListener);
     }
+    View.OnClickListener btnInputListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+            dlg.setIcon(R.drawable.별);
+            dlg.setTitle("사용자정보입력");
+            dlgView = View.inflate(MainActivity.this,R.layout.dialog1,null);
+            dlg.setView(dlgView);
+            dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dlgEditName = dlgView.findViewById(R.id.edit_name);
+                    dlgEditEmail = dlgView.findViewById(R.id.edit_email);
+                    textName.setText(dlgEditName.getText().toString());
+                    textEmail.setText(dlgEditEmail.getText().toString());
+                }
+            });
+            dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    toastView = View.inflate(MainActivity.this, R.layout.toast, null);
+                    Toast t = new Toast(MainActivity.this);
+                    t.setDuration(Toast.LENGTH_LONG);
+                    toastText = toastView.findViewById(R.id.text_toast);
+                    toastText.setText("Have nice day~~");
+                    t.setView(toastView);
+                    t.show();
+                }
+            });
+
+            dlg.show();
+        }
+    };
 }
